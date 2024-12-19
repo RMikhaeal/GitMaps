28f78e4d38c9c4a159e861b90903782cd8444b5e
Rayn Mikhaeal <rmikhaeal@gmail.com>
2024-12-19 12:52:47 -0500

    add fake data for name and location

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 class User {
   name: string;
   location: {
@@ -5,5 +7,11 @@ class User {
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.person.firstName();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
 }
